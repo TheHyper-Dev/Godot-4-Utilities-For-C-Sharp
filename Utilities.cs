7af4a24bf056d997d610716c38b5d831a4e7d62b
TheHyper-Dev <32967925+TheHyper-Dev@users.noreply.github.com>
2024-02-14 20:26:45 +0300

    Update Utilities.cs

--- Utilities.cs
+++ Utilities.cs
@@ -2,10 +2,11 @@ using Godot;
 
 public static class Physics
 {
-    static readonly StringName
+    private static readonly StringName
     position = "position",
     collider = "collider",
     collider_id = "collider_id",
+    collider_rid = "rid",
     normal = "normal",
     shape = "shape";
 
@@ -13,6 +14,7 @@ public static class Physics
     public static Vector3 CollisionNormal(Godot.Collections.Dictionary cast_result) => (Vector3)cast_result[normal];
     public static int CollisionShapeIndex(Godot.Collections.Dictionary cast_result) => (int)cast_result[shape];
     public static Variant Collider(Godot.Collections.Dictionary cast_result) => cast_result[collider];
-    public static Rid ColliderID(Godot.Collections.Dictionary cast_result) => (Rid)cast_result[collider_id];
+    public static int ColliderID(Godot.Collections.Dictionary cast_result) => (int)cast_result[collider_id];
+    public static Rid ColliderRID(Godot.Collections.Dictionary cast_result) => (Rid)cast_result[collider_rid];
     public static T Collider<T>(Godot.Collections.Dictionary cast_result) where T : CollisionObject3D => (T)cast_result[collider];
 }
